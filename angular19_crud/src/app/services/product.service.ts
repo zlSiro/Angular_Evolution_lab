df86871a62810dc77e36ceb3951c99c581fe90bc
JuanPablo <jvaldebenito@iplusd.cl>
2026-09-21 16:56:54 -0300

    feat: implementar métodos para gestionar productos en ProductService

--- angular19_crud/src/app/services/product.service.ts
+++ angular19_crud/src/app/services/product.service.ts
@@ -1,9 +1,51 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private products: Product[] = [
+    {
+      id: 1,
+      name: 'Teclado mecánico',
+      price: 89990
+    },
+    {
+      id: 2,
+      name: 'Mouse gamer',
+      price: 49990
+    },
+    {
+      id: 3,
+      name: 'Monitor 27"',
+      price: 199990
+    }
+  ];
+
+  getProducts(): Product[] {
+    return this.products;
+  }
+
+  addProduct(product: Product): void {
+    this.products.push(product);
+  }
+
+  deleteProduct(id: number): void {
+    this.products = this.products.filter(
+      product => product.id !== id
+    );
+  }
+
+  updateProduct(updatedProduct: Product): void {
+    const index = this.products.findIndex(
+      product => product.id === updatedProduct.id
+    );
+
+    if (index !== -1) {
+      this.products[index] = updatedProduct;
+    }
+  }
+
 }
