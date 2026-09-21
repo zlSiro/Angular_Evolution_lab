24350588c703b8699baa836919f282a8af891b99
JuanPablo <jvaldebenito@iplusd.cl>
2026-09-21 15:52:39 -0300

    feat: remove unused RouterOutlet import from InputDemo component

--- angular19_crud/src/app/components/input-demo/input-demo.component.ts
+++ angular19_crud/src/app/components/input-demo/input-demo.component.ts
@@ -1,9 +1,8 @@
 import { Component, input } from '@angular/core';
-import { RouterOutlet } from '../../../../node_modules/@angular/router/router_module.d-Bx9ArA6K';
 
 @Component({
   selector: 'app-input-demo',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './input-demo.component.html',
   styleUrl: './input-demo.component.css'
 })
