4f308f4ff6eceb5f6c64aa7577e575ca72f62a9e
JuanPablo <jvaldebenito@iplusd.cl>
2026-09-21 16:25:23 -0300

    feat: agregar componente ModelDemo con su plantilla y estilos

---
+++ angular19_crud/src/app/components/model-demo/model-demo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-model-demo',
+  imports: [],
+  templateUrl: './model-demo.component.html',
+  styleUrl: './model-demo.component.css'
+})
+export class ModelDemoComponent {
+
+}
