bde0a607955fa571bdc004706c3915abd0c58daf
JuanPablo <jvaldebenito@iplusd.cl>
2026-09-21 16:54:53 -0300

    feat: agregar componentes ProductForm y ProductList con sus plantillas y estilos

---
+++ angular19_crud/src/app/components/product-form/product-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-form',
+  imports: [],
+  templateUrl: './product-form.component.html',
+  styleUrl: './product-form.component.css'
+})
+export class ProductFormComponent {
+
+}
