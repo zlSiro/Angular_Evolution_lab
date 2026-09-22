78465ce3617be16bb795073ff4664dd8cd0f93bc
JuanPablo <jvaldebenito@iplusd.cl>
2026-09-22 16:35:47 -0300

    refactor: eliminar importación innecesaria de RouterOutlet en AppComponent

--- angular20_crud/src/app/app.ts
+++ angular20_crud/src/app/app.ts
@@ -1,9 +1,8 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
