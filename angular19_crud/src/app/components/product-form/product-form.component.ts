dcad8e2e8a23cb9fd87f237d76300d666e918d6f
JuanPablo <jvaldebenito@iplusd.cl>
2026-09-22 14:15:27 -0300

    feat: implementar formulario de producto con campos para nombre, descripción, precio y stock

--- angular19_crud/src/app/components/product-form/product-form.component.ts
+++ angular19_crud/src/app/components/product-form/product-form.component.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import { Product } from '../../models/product';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './product-form.component.html',
   styleUrl: './product-form.component.css'
 })
@@ -15,5 +16,13 @@ export class ProductFormComponent {
   // Angular 19:
   // Signal Input estable desde Angular 19.
   productToEdit = input<Product | null>(null);
+
+  product: Product = {
+    id: 0,
+    name: '',
+    description: '',
+    price: 0,
+    stock: 0
+  };
 
 }
